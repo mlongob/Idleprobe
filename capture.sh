5484dfcdb7ab8c964057d3524bf33d0f52f97794
Mario Longobardi <mlongob@gmail.com>
2012-04-09 05:35:52 -0400

    Auto commit (commit.sh)

--- capture.sh
+++ capture.sh
@@ -3,14 +3,12 @@
 while [ 1 ]
 do
 	FILENAME=$(date +"idleprobe-%s.cap")
-	echo $FILENAME
 	SIZE=0
-	echo $SIZE
 	while [ $SIZE -lt 1048576 ]
 	do
 		cat /proc/idleprobe >> $FILENAME
 		sleep 1
 		SIZE=$( stat -c %s $FILENAME)
-		echo $SIZE
 	done
+	echo $SIZE bytes wrote to $FILENAME
 done
