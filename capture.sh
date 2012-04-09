9d3b6700c4724852e323dcced1051c05363a5199
Mario Longobardi <mlongob@gmail.com>
2012-04-09 06:19:49 -0400

    Auto commit (commit.sh)

--- capture.sh
+++ capture.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
+if [ ! -r /proc/idleprobe ]
+then
+	echo "Error: the idleprobe module is not running."
+	exit 1
+fi
 while [ 1 ]
 do
 	FILENAME=$(date +"idleprobe-%s.cap")
@@ -7,6 +11,11 @@ do
 	echo Writing $FILENAME:
 	while [ $SIZE -lt 1048576 ]
 	do
+		if [ ! -r /proc/idleprobe ]
+		then
+			echo "Error: the idleprobe module is not running."
+			exit 1
+		fi
 		cat /proc/idleprobe >> $FILENAME
 		sleep 30
 		SIZE=$( stat -c %s $FILENAME)
